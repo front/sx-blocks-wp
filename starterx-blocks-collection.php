51a7c3cb891b8f7950fdcc5b6075bcb492981841
Sofia Sousa <csofiamsousa@gmail.com>
2019-03-12 15:07:20 +0000

    First commit

---
+++ starterx-blocks-collection.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Starterx Blocks Collection
+ * Author: Frontkom
+ * Author URI: https://frontkom.no
+ * Version: 1.0.0
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
